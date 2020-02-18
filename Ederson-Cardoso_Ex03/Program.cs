eff81a54b4710a096648a3ec902f85e7963e62f8
EderCard <edersoncard@gmail.com>
2020-02-18 14:18:04 -0500

    Add Ex3

--- Ederson-Cardoso_Ex03/Program.cs
+++ Ederson-Cardoso_Ex03/Program.cs
@@ -10,6 +10,65 @@ namespace Ederson_Cardoso_Ex03
     {
         static void Main(string[] args)
         {
+            WordsDictionary();
+        }
+
+        public static void WordsDictionary()
+        {
+            Console.Write("Enter a sentence: ");
+            string sentence = Console.ReadLine();
+
+            string[] words = sentence.Split(' ');
+
+            // Create Dictionaty
+            Dictionary<int, string> dict = new Dictionary<int, string>();
+
+            // Populate Dictionary
+            for (int i = 0; i < words.Length; i++)
+            {
+                dict.Add(i, words[i]);
+            }
+
+            // Count word occurence
+            var wc = new Dictionary<string, int>();
+            foreach (string w in words)
+            {
+                if (wc.ContainsKey(w))
+                { 
+                    wc[w]++;
+                }
+                else
+                {                   
+                    wc[w] = 1;
+                }
+            }
+
+            Console.WriteLine("Sorted by Value");
+            foreach (KeyValuePair<int, string> w in dict.OrderBy(key => key.Value))
+            {
+                Console.WriteLine("Key: {0}, Value: {1}", w.Key, w.Value);
+            }
+
+            // Display Dictionary content
+            Console.WriteLine("Dictionary content:");
+            var str = new Dictionary<string, int>();
+            foreach (string w in words)
+            {
+                Console.WriteLine(w);
+            }
+
+            // Print countec words
+            List<string> str1 = new List<string>(wc.Keys);
+            str1.Sort();
+            foreach (string w in str1)
+            {
+                if (wc[w] >= 0)
+                {
+                    Console.WriteLine("{0}: {1}", w, wc[w]);
+                }
+            }
+
+            Console.ReadKey();
         }
     }
 }
